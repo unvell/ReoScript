acee60191213ee2a091b27fcb5ca103bffc01922
Jingwood <dujid0@gmail.com>
2019-11-24 21:06:06 +0900

    update samples

--- Samples/DirectAccess/DirectAccessForm.Designer.cs
+++ Samples/DirectAccess/DirectAccessForm.Designer.cs
@@ -32,40 +32,65 @@
 			this.button1 = new System.Windows.Forms.Button();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(353, 194);
+			this.button1.Location = new System.Drawing.Point(929, 498);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(124, 70);
+			this.button1.Size = new System.Drawing.Size(199, 58);
 			this.button1.TabIndex = 0;
-			this.button1.Text = "Run";
+			this.button1.Text = "Run Script";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(51, 194);
+			this.textBox1.Location = new System.Drawing.Point(929, 345);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(275, 70);
+			this.textBox1.Size = new System.Drawing.Size(354, 136);
 			this.textBox1.TabIndex = 1;
-			this.textBox1.Text = "guest.nickname=\'player1\';\r\nguest.hello();";
+			this.textBox1.Text = "user.nickname=\'player1\';\r\nuser.hello();";
 			// 
 			// label1
 			// 
-			this.label1.Location = new System.Drawing.Point(20, 20);
+			this.label1.Location = new System.Drawing.Point(12, 19);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(457, 155);
+			this.label1.Size = new System.Drawing.Size(872, 166);
 			this.label1.TabIndex = 2;
-			this.label1.Text = resources.GetString("label1.Text");
+			this.label1.Text = "DirectAccess provide the ability that makes script can access property, call meth" +
+    "od from a .NET object directly.\r\n\r\nTo enable DirectAccess:\r\n\r\nsrm.WorkMode |= Ma" +
+    "chineWorkMode.AllowDirectAccess;";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(16, 345);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(868, 576);
+			this.richTextBox1.TabIndex = 3;
+			this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 256);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(1018, 72);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "User class and its properties are defined in C# but available in both C# and scri" +
+    "pt.\r\n\r\nSource code:\r\n";
 			// 
 			// DirectAccessForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(517, 293);
+			this.ClientSize = new System.Drawing.Size(1408, 984);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.button1);
@@ -82,6 +107,8 @@
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label2;
 	}
 }
 
